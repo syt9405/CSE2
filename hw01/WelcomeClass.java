09a85457a1aef402e7a91e727ebdd5bf6670a5b3
syt9405 <yus317@lehigh.edu>
2014-08-31 16:34:08 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Yutong Song
+//Demo 
+//WelcomeClass Java Program
+//javac 
+// first compile the program
+//       javac WelcomeClass.java
+// run the program
+//       java WelcomeClass
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+  public static void main(String[] args) {
+    
+// print the statement with box around it
+    System.out.println("  -------------");
+    System.out.println("  |  WELCOME  |");
+    System.out.println("  -------------"); 
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-Y--U--S--3--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("Hey, I just met you");
+    System.out.println("This is crazy");
+    
+    //test/testtest 
+    }
+}
