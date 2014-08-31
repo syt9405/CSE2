187f16aa571f3c9412c7863f506cb49804c44e1c
syt9405 <yus317@lehigh.edu>
2014-08-31 15:32:55 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+//Yutong Song 
+//Hello World Java Program
+//
+// first compile the program
+//       javac HelloWorld.java
+// run the program
+//       java HelloWorld//
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+  public static void main(String[] args) {
+    
+// print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello world 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest 
+    }
+}
